5daf2db155c22360423e7d690983745e06bea4e6
Yuki KAN <re@pixely.jp>
2017-04-15 17:11:05 +0900

    Setup/build.bat: revert

--- Setup/build.bat
+++ Setup/build.bat
@@ -1,5 +1,5 @@
-rem call .\sign-releases.bat
+call .\sign-releases.bat
 "C:\Program Files (x86)\WiX Toolset v3.7\bin\candle.exe" YabumiUploader.wxs
 "C:\Program Files (x86)\WiX Toolset v3.7\bin\light.exe" -ext WixUIExtension YabumiUploader.wixobj
-rem call .\sign-installer.bat
+call .\sign-installer.bat
 pause
